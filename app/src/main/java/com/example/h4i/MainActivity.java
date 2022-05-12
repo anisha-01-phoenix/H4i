12aff72ceb8e5a4db6fcf46fc22522886e966fbe
anisha-01-phoenix <anisha.dat01@gmail.com>
2022-05-13 01:08:56 +0530

    init

---
+++ app/src/main/java/com/example/h4i/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.h4i;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
