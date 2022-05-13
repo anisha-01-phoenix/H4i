e40f5344160d26a15d0119043bc3f4d3bd1dbb9f
anisha-01-phoenix <anisha.dat01@gmail.com>
2022-05-14 02:53:26 +0530

    Home Page Design + Doctors' List

---
+++ app/src/main/java/com/example/h4i/Activity/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.h4i.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.h4i.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.profile) {
+            //Profile Page
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
